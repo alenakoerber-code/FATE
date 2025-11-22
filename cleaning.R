fd6997943f16dff762aeda32c231f8019f750202
Alena Koerber <alena_koerber@gmx.de>
2025-11-22 17:42:20 +0100

    add columns hours to fate: cat, lower and upper harmonising value names hours to fate

--- cleaning.R
+++ cleaning.R
@@ -1,3 +1,5 @@
+# intro -------
+
 library("renv")
 pacman::p_load(
   rio,        # importing data  
@@ -6,19 +8,20 @@ pacman::p_load(
   lubridate,  # working with dates
   matchmaker, # dictionary-based cleaning
   epikit,     # age_categories() function
-  tidyverse,   # data management and visualization
+  tidyverse,  # data management and visualization
   skimr,
-  readxl
+  readxl,      # reads excel datasets
+  units
 )
 
 
-# IMPORT ---------------
+# IMPORT 
 linelist_raw <- import("linelist_FATE_raw.xlsx", range = cell_rows(4:44))
 
 
-# CLEANING -----
+# cleaning --------
 
-## cleaning column names
+## name cleaning ------
 linelist <- linelist_raw %>%
   
   janitor::clean_names() %>% 
@@ -29,44 +32,62 @@ linelist <- linelist_raw %>%
          gender = m_w,
          age = alter,
          location_fate = ort,
-         time_to_fate_h = zeitpunkt,
+         hours_to_fate = zeitpunkt,
          reason_fate = indikation,
          clinical_diagn = diagnose,
          outcome_fate = fate_befund,
          consequence = konsequenz,
          examiner = untersucher,
          advanced_echo_followed = tte_im_verlauf_erfolgt,
-         time_to_adv_echo = zeipunkt,
+         hours_to_adv_echo = zeipunkt,
          outcome_adv_echo = befund
          ) 
   
-## clean values
+## value cleaning -------
   skimr::skim(linelist)
   
-  linelist <- linelist %>% 
-  ### Patient names for pseudonymisation
-  mutate(
-    patient_name = recode(patient_name, 
+  linelist <- linelist %>%
+    ### recode values ---------
+      #### Patient names for pseudonymisation
+        mutate(
+          patient_name = recode(patient_name, 
                     "Rahaus, Wolfgang (Le) II" = "Rahaus, Wolfgang",
                     "Rahaus, Wolfgang (Le) I" = "Rahaus, Wolfgang"  
                     )
-      ) %>%
-  ### gender
-  mutate(
-    gender = recode(gender,
+              ) %>%
+      #### gender
+        mutate(
+            gender = recode(gender,
                     "m" = "male",
-                    "w" = "female"
+                    "w" = "female",
+                    .default = "other"
                     )
-          ) %>% 
+              ) %>% 
+      #### hours to fate
+        mutate(
+          hours_to_fate = recode(hours_to_fate,
+                                 "bis 2 h" = "< 2",
+                                 "2 bis 12h" = "2 - 12",
+                                 ">12" = "> 12"
+                                 )
+           
+        ) %>% 
   
-  ### date as date format
-  mutate(    date_fate  = as.Date(date_fate))
+  ### set class
+    
+    #### date as date format
+    mutate(date_fate  = as.Date(date_fate)) %>% 
   
-  ### missing values
+  ### missing values ---------
+    #### fate_date replaced with median date
+    mutate (
+      date_fate = replace_na (
+                date_fate, median(date_fate, na.rm = TRUE) 
+                             )
+          )
   
   
-  
-  # pseudoonymisation ------
+  # pseudonymisation ------
   set.seed(101)
 
   ## create key
@@ -79,11 +100,11 @@ id_key <- linelist %>%
   ## replace names with id
   linelist <- linelist %>%
     left_join(id_key, by = "patient_name") %>%
-     select(-patient_name)   # Namen entfernen
+     select(-patient_name)   # delete names
   
-# add columns
+# add columns -------
   linelist <- linelist %>% 
-  ## diagnosis group
+  ## diagnosis group -------
   mutate(
     clinical_diagn_grp = recode(clinical_diagn,
                                 "Z.n. Schrittmachernendokarditis - unklares Fieber" = "fever",
@@ -108,14 +129,43 @@ id_key <- linelist %>%
                                 .default = "other"
     )
     ) %>% 
-  ## age categories: 0 to 85 by 5s
-  mutate( age_cat = epikit::age_categories(age, breakers = c(0, 40, 50, 60, 70, 80, 90)))
+  ## age categories: manual ------
+  mutate( age_cat = epikit::age_categories(age, breakers = c(0, 40, 50, 60, 70, 80, 90))) %>% 
   
+  
+  ## hours to fate ----------
+    mutate(
+      hours_to_fate_cat = case_when(
+        hours_to_fate == "< 2"   ~ 1,
+        hours_to_fate == "2 - 12"  ~ 2,
+        hours_to_fate == "> 12"    ~ 3,
+        TRUE ~ NA_real_
+      )
+    ) %>% 
+  
+    mutate(
+      lower_hours_fate = case_when(
+        hours_to_fate == "< 2" ~ 0,
+        hours_to_fate == "2 - 12" ~ 2,
+        hours_to_fate == "> 12" ~ 12,
+        TRUE ~ NA_real_
+        ),
+      upper_hours_fate = case_when(
+        hours_to_fate == "< 2" ~ 2,
+        hours_to_fate == "2 - 12" ~ 12,
+        hours_to_fate == "> 12" ~ Inf,
+        TRUE ~ NA_real_
+      )
+          )
   
 # column order ----
 ## delete columns
   linelist <- linelist %>% 
     select(-c(x16)) %>% 
 ## rearrange columns
-    select(patient_id, case_id, gender, age, age_cat, clinical_diagn, clinical_diagn_grp, reason_fate, outcome_fate, consequence, location_fate, date_fate, time_to_fate_h, everything())
+    select(
+      patient_id, case_id, gender, age, age_cat, 
+      clinical_diagn, clinical_diagn_grp, reason_fate, outcome_fate, consequence, 
+      location_fate, date_fate, hours_to_fate, hours_to_fate_cat, lower_hours_fate, upper_hours_fate, 
+      everything())
 
