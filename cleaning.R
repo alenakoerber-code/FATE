f5fe759a489131bd51c8a87c15c66c325720806e
Alena Koerber <alena_koerber@gmx.de>
2025-11-22 15:41:42 +0100

    pseudonimisation, string cleaning

--- cleaning.R
+++ cleaning.R
@@ -15,7 +15,6 @@ pacman::p_load(
 # IMPORT ---------------
 linelist_raw <- import("linelist_FATE_raw.xlsx", range = cell_rows(4:44))
 
-skimr::skim(linelist_raw)
 
 # CLEANING -----
 
@@ -24,22 +23,87 @@ linelist <- linelist_raw %>%
   
   janitor::clean_names() %>% 
   
-  rename(ID_case = patient,
+  rename(case_id = patient,
          date_fate = x2,
+         patient_name = name,
          gender = m_w,
          age = alter,
          location_fate = ort,
-         time_to_fate = zeitpunkt,
+         time_to_fate_h = zeitpunkt,
          reason_fate = indikation,
-         clinical_diagnosis = diagnose,
+         clinical_diagn = diagnose,
          outcome_fate = fate_befund,
          consequence = konsequenz,
          examiner = untersucher,
          advanced_echo_followed = tte_im_verlauf_erfolgt,
          time_to_adv_echo = zeipunkt,
          outcome_adv_echo = befund
-         ) %>% 
-
-# delete columns
-select(-c(x16))
+         ) 
   
+## clean values
+  skimr::skim(linelist)
+  
+  linelist <- linelist %>% 
+  ### Patient names for pseudonymisation
+  mutate(
+    patient_name = recode(patient_name, 
+                    "Rahaus, Wolfgang (Le) II" = "Rahaus, Wolfgang",
+                    "Rahaus, Wolfgang (Le) I" = "Rahaus, Wolfgang"  
+                    )
+      ) %>%
+  ### gender
+  mutate(
+    gender = recode(gender,
+                    "m" = "male",
+                    "w" = "female")
+          ) %>% 
+  ### diagnosis
+  mutate(
+    clinical_diagn = recode(clinical_diagn,
+                            "Z.n. Schrittmachernendokarditis - unklares Fieber" = "fever",
+                            "Herzinsuffizienz/ Pleuraerguss" = "congestive heart failure",
+                            "Herzinsuffizienz" = "congestive heart failure",
+                            "LV-Hypertrophie/atriale Dil." = "hypertension",
+                            "hypertensive Entgleisung / VHF/ cor hypertensivum" = "hypertension",
+                            "Herzinsuffizienz/COPD" = "congestive heart failure",
+                            "Herzinsuffizienz/Pleuraerguss" = "congestive heart failure",
+                            "Thoraxschmerzen" = "chest pain",
+                            "Normalbefund" = "normal",
+                            "Exsikkose" = "dehydration",
+                            "Normozytäre Anämie" = "anemia",
+                            "TAA / Herzinsuffizienz" = "arrhythmia",
+                            "VHF / Herzinsuffizienz" = "arrhythmia",
+                            "TAA bei VHF" = "arrhythmia",
+                            "Herzrhythmusstörung" = "arrhythmia",
+                            "Herzinsuffizienz / TAA bei VHF" = "arrhythmia",
+                            "V.a. LAE" = "pulmonary embolism",
+                            "Lungenarterienembolie" = "pulmonary embolism",
+                            "hypertensives Lungenödem" = "hypertension",
+                            .default = "other"
+                            )
+  )
+  
+  # pseudoonymisation ------
+  set.seed(101)
+
+  ## create key
+id_key <- linelist %>%
+  distinct(patient_name) %>% 
+  mutate(
+    patient_id = sample(101:999, size = n(), replace = TRUE)
+  )
+  
+  ## replace names with id
+  linelist <- linelist %>%
+    left_join(id_key, by = "patient_name") %>%
+     select(-patient_name)   # Namen entfernen
+
+# column order ----
+## delete columns
+  linelist <- linelist %>% 
+    select(-c(x16)) %>% 
+## rearrange columns
+    relocate(patient_id) %>% 
+    relocate(date_fate, .after = location_fate) %>% 
+    relocate (outcome_fate, .after = reason_fate) %>% 
+    relocate (clinical_diagn, .after = age)
